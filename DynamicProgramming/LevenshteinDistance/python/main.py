095c8831167622988cbb089e8a7fa6cb0c8a80e2
Christian Leininger <info2016frei@gmail.com>
2019-12-27 07:40:41 +0100

    first recursive implementation

---
+++ DynamicProgramming/LevenshteinDistance/python/main.py
@@ -0,0 +1,36 @@
+
+
+# time O(3^n-1) | space O(1)
+def levenshteinDistance(str1, len_str1, str2, len_str2):
+    """ Computes the levenshteinDistance between the two
+    given strings using recusion. If the last char are the same cost 0
+    else 1 
+
+    Args:
+        param1: str
+        param2: str
+    Return: int represents the levenshteinDistance
+
+    """
+
+    # base cases 
+    if len_str1 == 0:
+        return len_str2
+    
+    if len_str2 == 0:
+        return len_str1
+    
+    if str1[len_str1] == str2[len_str2]:
+        cost = 0
+    else:
+        cost = 1
+
+    return min(levenshteinDistance(str1[:-1], len_str1 - 1, str2, len_str2) + 1,
+            levenshteinDistance(str1, len_str1 , str2[:-1], len_str2 - 1) + 1,
+            levenshteinDistance(str1[:-1], len_str1 - 1, str2[:-1], len_str2 - 1) + cost)
+
+
+
+if __name__ == "___main__":
+    import doctest
+    doctest.testmod()
