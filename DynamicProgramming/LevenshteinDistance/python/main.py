21fb5cf30d047ef1cec0ae6648c6a2d1d552af19
Christian Leininger <info2016frei@gmail.com>
2019-12-27 11:15:53 +0100

    dp of lev dist

--- DynamicProgramming/LevenshteinDistance/python/main.py
+++ DynamicProgramming/LevenshteinDistance/python/main.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 # time O(3^n-1) | space O(1)
 def levenshteinDistance(str1, len_str1, str2, len_str2):
@@ -18,6 +18,10 @@ def levenshteinDistance(str1, len_str1, str2, len_str2):
     >>> s = "h"
     >>> levenshteinDistance(w, len(w)-1, s, len(s) - 1)
     4
+    >>> w = "kitten"
+    >>> s = "siting"
+    >>> levenshteinDistance(w, len(w)-1, s, len(s) - 1)
+    3
     """
 
     # base cases 
@@ -35,6 +39,55 @@ def levenshteinDistance(str1, len_str1, str2, len_str2):
     return min(levenshteinDistance(str1[:-1], len_str1 - 1, str2, len_str2) + 1,
             levenshteinDistance(str1, len_str1 , str2[:-1], len_str2 - 1) + 1,
             levenshteinDistance(str1[:-1], len_str1 - 1, str2[:-1], len_str2 - 1) + cost)
+
+
+
+
+
+
+# timeO(n+m) | Space O(n+m)
+def levenshteinDistance1(str1, str2):
+    """Use Dynamic Programming to reduce complexity
+
+    Args:
+        param1: str
+        param2: str
+    Return: int represents the levenshteinDistance
+    >>> w = " "
+    >>> s = " "
+    >>> levenshteinDistance1(w, s)
+    0
+    >>> w = "hello"
+    >>> s = "h"
+    >>> levenshteinDistance1(w, s)
+    4
+    >>> w = "kitten"
+    >>> s = "sitting"
+    >>> levenshteinDistance1(w, s)
+    3
+    """
+
+    sol = np.zeros((len(str1) + 1, len(str2) + 1))
+    for i in range(sol.shape[0]):
+        # sol[i,j] # row i # columon j
+        sol[i, 0] = i
+    for j in range(sol.shape[1]):
+        # sol[i,j] # row i # columon j
+        sol[0, j] = j
+    str1 = " " + str1
+    str2 = " " + str2
+    for j in range(1, sol.shape[1]):
+        for i in range(1, sol.shape[0]):
+            if str1[i] == str2[j]:
+                cost = 0
+            else:
+                cost = 1
+            sol[i, j] = min(sol[i-1, j] + 1, sol[i, j-1] + 1, sol[i-1, j-1] + cost)
+    return int(sol[sol.shape[0]-1, sol.shape[1]-1])
+
+
+
+
 
 
 
