ac04d5d51e168ff5ba443af4d10916bcbaa28890
Christian Leininger <info2016frei@gmail.com>
2019-12-30 11:07:21 +0100

    add docstrings

--- Sort/QuickSort/python/program.py
+++ Sort/QuickSort/python/program.py
@@ -1,27 +1,46 @@
 import numpy as np
 
 
+# O(n log n) time | O(log n) space
+def quickSort(array, if_random=True):
+    """ Sort the given input array using the Quicksort algorithm (divide and
+    conquer method by using the left element as pivot the worst case is O(n^2)
+    when the array is already sorted
+    Chossing the pivot uniformly at random the time complexity is O(n log(n))
+    where n is the length of the array
 
-def quickSort(array):
-    """ 
+    Args:
+       param1: list of int
+       param2: bool default Ture use random pivot
+
+
+    Return: sorted array 
 
     """
+    # case if array is empty or has only one element
     if len(array) < 2:
         return array
     start = 0
-    end = len(array) - 1
-    sorted_array = recursion(array, start, end)
+    end = len(array) - 1  # list starts at 0
+    sorted_array = recursion(array, start, end, if_random)
     return sorted_array
 
 
 
 
 
-def  recursion(array, left, right):
+def  recursion(array, left, right, if_random=True):
+    """ Calls the divide part recusivly on the left and right part of the pivot
+    element
+    Args:
+        param1: array (list) sub array
+        param2: left (int)   left boarder
+        param3: right (int)  right boarder
+        param4: if_random (bool)
+
+    Return:
     """
-    
-    """
-    #print("left {} right {}".format(left, right))
+    # base case if the sub array contains only 1 element
     if left >=right:
         return 
     pos = helper(array, left, right)
@@ -34,9 +53,16 @@ def  recursion(array, left, right):
 
 
 def helper(array, left, right, if_random=True):
-    """
+    """ Sort sub array by choosing the pivot and 
+         bring the elements on the correct side 
+         set the pivot element om the final postion and
+         return this position
+
     
     Args:
+        param1: array (list of int) subarray
+        param2: left (int) left boundry
+        param3: rigth(int)
     
     
     Return:
@@ -61,9 +87,6 @@ def helper(array, left, right, if_random=True):
         while rp >= lp and array[rp] > pivot:
             rp -=1
 
-        #print("left {} right {}".format(lp, rp))
-        #print("left {} right {}".format(array[lp], array[rp]))
-        #print("pivo {}".format(array[pos]))
         if lp >= rp:
             break
         # now lp points on element greater than pivot and rp on smaller
