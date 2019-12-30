6a118fd51653a0f2c1daa380fb20e62a33a41495
Christian Leininger <info2016frei@gmail.com>
2019-12-30 15:28:54 +0100

    add strcut of MinHeap class

---
+++ Heaps/MinHeap/python/program.py
@@ -0,0 +1,31 @@
+# Do not edit the class below except for the buildHeap,
+# siftDown, siftUp, peek, remove, and insert methods.
+# Feel free to add new properties and methods to the class.
+class MinHeap:
+    def __init__(self, array):
+        # Do not edit the line below.
+        self.heap = self.buildHeap(array)
+
+    def buildHeap(self, array):
+        # Write your code here.
+        pass
+
+    def siftDown(self):
+        # Write your code here.
+        pass
+
+    def siftUp(self):
+        # Write your code here.
+        pass
+
+    def peek(self):
+        # Write your code here.
+        pass
+
+    def remove(self):
+        # Write your code here.
+        pass
+
+    def insert(self, value):
+        # Write your code here.
+        pass
