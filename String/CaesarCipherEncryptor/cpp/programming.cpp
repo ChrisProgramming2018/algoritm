e4384eb1eb4406c11f6a87540dcb5ac26e0c3bd9
Christian Leininger <info2016frei@gmail.com>
2020-01-01 00:51:57 +0100

    add caesar cipher encoder cpp

---
+++ String/CaesarCipherEncryptor/cpp/programming.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <numeric>
+
+
+
+char getNewLetter(char letter, int key);
+
+
+// O(n) time | O(n) space
+std::string caesarCypherEncryptor(std::string str, int key) {
+  std::vector<char> newLetter;
+  int newKey = key % 26;
+  for (int i = 0; i < str.length(); i++) {
+    newLetter.push_back(getNewLetter(str[i], newKey));
+  }
+  return string(newLetter.begin(), newLetter.end());
+}
+
+char getNewLetter(char letter, int key) {
+  int newLetterCode = letter + key;
+  return newLetterCode <= 122 ? newLetterCode : 96 + newLetterCode % 122;
+}
+
+
+char getNewLetter(char letter, int key, std::string alphabet);
+
+// O(n) time | O(n) space
+std::string caesarCypherEncryptor2(std::string str, int key) {
+  std::vector<char> newLetter;
+  int newKey = key % 26;
+  std::string alphabet = "abcdefghijklmnopqrstuvxyz";
+  for ( int i = 0; i < str.length(); i++) {
+    newLetter.push_back(getNewLetter2(str[i], newKey, alphabet));
+  }
+  return std::string(newLetter.begin(), newLetter.end());
+}
+
+char getNewLetter2(char letter, int key, std::string alphabet) {
+  int newLetterCode = alphabet.find(letter) + key;
+  return newLetterCode <= 25 ? alphabet[newLetterCode] : alphabet[-1 + newLetterCode % 25];
+}
