af95842c78ad31be4b7ee5bd5ea2b32a9f907eae
Christian Leininger <info2016frei@gmail.com>
2020-01-06 10:20:10 +0100

    add doc string and test

--- Recursion/ProductSum/python/program.py
+++ Recursion/ProductSum/python/program.py
@@ -5,12 +5,18 @@
 
 def productSum(array, level=1):
     """
-
+    Compute the sum of the elements and multiply it by the depth level
+    O(n) time | O(d) space n := total number in input array(count also elements
+    in all subarrays d := is largest depth subarray in the input array
 
     Args:
         param1:(list) array  of integers
         param2:(int) level represent the depth of the nested list
     Return: int represent the productSum
+
+    >>> array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
+    >>> productSum(array)
+    12
     """
     res = 0  # init the sum
 
