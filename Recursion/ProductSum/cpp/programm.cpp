3ba36b70ffd7f10f64f3995b577adf8f8e5c9d64
Christian Leininger <info2016frei@gmail.com>
2020-01-06 10:34:40 +0100

    add comments

--- Recursion/ProductSum/cpp/programm.cpp
+++ Recursion/ProductSum/cpp/programm.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 
 
-
+// O(n) time | O(d) space n := input elements(subarrays also) 
+// d is the max depth of a sub array where the elements in Input array is 1
 int productSum(std::vector<std::any> array, int level=1) {
   int sum = 0;
   for (auto el : array) {
