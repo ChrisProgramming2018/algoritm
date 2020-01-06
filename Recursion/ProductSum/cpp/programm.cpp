0d058206cad076f0c13c0470b7c48e6bc693cbcb
Christian Leininger <info2016frei@gmail.com>
2020-01-06 10:45:01 +0100

    add comments and fix bugs

--- Recursion/ProductSum/cpp/programm.cpp
+++ Recursion/ProductSum/cpp/programm.cpp
@@ -7,11 +7,17 @@
 int productSum(std::vector<std::any> array, int level=1) {
   int sum = 0;
   for (auto el : array) {
-    if (el.type() == typeid(std::vector<any>)) {
-      sum += productSum(el, level + 1);
+    // check if element is also an array call recursively productSum
+    if (el.type() == typeid(std::vector<std::any>)) {
+      sum += productSum(std::any_cast<std::vector<std::any>>(el), level + 1);
      } else {
+       // in case its an int cast and add
        sum += std::any_cast<int>(el);
      }
   }
   return sum * level;
+}
+
+int main() {
+
 }
