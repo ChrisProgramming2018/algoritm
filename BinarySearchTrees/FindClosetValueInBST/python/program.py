c3ea0cd5958d36632734344fd0747af10525de1f
Christian Leininger <info2016frei@gmail.com>
2020-01-07 11:05:03 +0100

    BST find closest value

--- BinarySearchTrees/FindClosetValueInBST/python/program.py
+++ BinarySearchTrees/FindClosetValueInBST/python/program.py
@@ -2,4 +2,12 @@
 
 def findClosestValueInBst(tree, target):
     """
+    
+    Args:
+       param1:(class BST ) tree value left right
+       param2:(int) target
+
+    Return: the closet value in the BST 
     """
+
+
