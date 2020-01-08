ae45d925038b10bb4a5e7d041613cb6e96844254
Christian Leininger <info2016frei@gmail.com>
2020-01-08 15:53:33 +0100

    checkstyle selection sort

--- Sort/SeclectionSort/python/progam.py
+++ Sort/SeclectionSort/python/progam.py
@@ -1,13 +1,16 @@
 import numpy as np
 
+
 def selectionSort(array):
-    """
-    
+    """ Use selection sort to sort array in place
+    Best case O(n^2) time | O(1) space
+    Average case O(n^2) time | O(1) space
+    Worst case O(n^2) time | O(1) space
+
     Args:
-        param1: array 
+        param1: array
     Return: sorted array
     """
-
     current_pos = 0
     length_array = len(array) - 1
     while current_pos < length_array:
@@ -16,11 +19,11 @@ def selectionSort(array):
         min_pos = current_pos
         sec = current_pos
         while True:
-            if  array[sec] < min_element:
+            if array[sec] < min_element:
                 min_element = array[sec]
                 min_pos = sec
-            sec +=1
-            
+            sec += 1
+
             if sec > length_array:
                 break
         # swap minimum element with current element
@@ -29,6 +32,6 @@ def selectionSort(array):
     print(array)
 
 if __name__ == "__main__":
-    array = np.random.randint(0,10**2, size=20)
+    array = np.random.randint(0, 10**2, size=20)
     print(array)
     selectionSort(array)
