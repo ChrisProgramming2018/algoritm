8b8b8e0175c129cffc48ba1ba531259232ea5420
Christian Leininger <info2016frei@gmail.com>
2020-01-08 18:05:07 +0100

    add depth first search python

---
+++ Graphs/DepthFirstSearch/python/program.py
@@ -0,0 +1,42 @@
+# create a class that represents a tree and has the depth first function to
+# traverses the nodes
+
+
+class Node:
+    def __init__(self, name):
+        """
+        Save the direct childern of the node and the name of the name
+        Args:
+            param1: (string) name of the Node
+        """
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        """ adds a child to the node
+        Args:
+            param1:(string) name of the node
+        Return its self
+        """
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        """ Travers the tree in a depth first fashion
+        O(n+e) time | O(n) space n := amount of nodes in the input tree
+        e := amount of edges in the input tree
+
+        Args:
+            param1:(list) array of the node
+        Returns: list of the names in the depth first order
+        """
+
+        array.append(self.name)
+
+        #  base case if a node has no children (leaf Node)
+        if len(self.children) == 0:
+            return array
+        for child in self.children:
+            # call the function recursively
+            child.depthFirstSearch(array)
+        return array
---
+++ Graphs/DepthFirstSearch/python/test.py
@@ -0,0 +1,62 @@
+# Add, edit, or remove tests in this file.
+# Treat it as your playground!
+
+import program
+import unittest
+
+
+test1 = program.Node("A")
+test1.addChild("B").addChild("C")
+test1.children[0].addChild("D")
+
+test2 = program.Node("A")
+test2.addChild("B").addChild("C").addChild("D").addChild("E")
+test2.children[1].addChild("F")
+
+test3 = program.Node("A")
+test3.addChild("B")
+test3.children[0].addChild("C")
+test3.children[0].children[0].addChild("D").addChild("E")
+test3.children[0].children[0].children[0].addChild("F")
+
+test4 = program.Node("A")
+test4.addChild("B").addChild("C").addChild("D")
+test4.children[0].addChild("E").addChild("F")
+test4.children[2].addChild("G").addChild("H")
+test4.children[0].children[1].addChild("I").addChild("J")
+test4.children[2].children[0].addChild("K")
+
+test5 = program.Node("A")
+test5.addChild("B").addChild("C").addChild("D").addChild("L").addChild("M")
+test5.children[0].addChild("E").addChild("F").addChild("O")
+test5.children[1].addChild("P")
+test5.children[2].addChild("G").addChild("H")
+test5.children[0].children[0].addChild("Q").addChild("R")
+test5.children[0].children[1].addChild("I").addChild("J")
+test5.children[2].children[0].addChild("K")
+test5.children[4].addChild("S").addChild("T").addChild("U").addChild("V")
+test5.children[4].children[0].addChild("W").addChild("X")
+test5.children[4].children[0].children[1].addChild("Y").addChild("Z")
+
+
+class TestProgram(unittest.TestCase):
+
+	def test_case_1(self):
+		self.assertEqual(test1.depthFirstSearch([]), ["A", "B", "D", "C"])
+
+	def test_case_2(self):
+		self.assertEqual(test2.depthFirstSearch([]), ["A", "B", "C", "F", "D", "E"])
+
+	def test_case_3(self):
+		self.assertEqual(test3.depthFirstSearch([]), ["A", "B", "C", "D", "F", "E"])
+
+	def test_case_4(self):
+		self.assertEqual(test4.depthFirstSearch([]), ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"])
+
+	def test_case_5(self):
+		self.assertEqual(test5.depthFirstSearch([]), ["A", "B", "E", "Q", "R", "F", "I", "J", "O", "C", "P", "D", "G", "K", "H", "L", "M", "S", "W", "X", "Y", "Z", "T", "U", "V"])
+
+
+if __name__ == "__main__":
+	unittest.main()
+
