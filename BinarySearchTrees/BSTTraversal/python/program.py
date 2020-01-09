f41a722a69fcb7e1576aae4a4bb0ec380058a1b6
Christian Leininger <info2016frei@gmail.com>
2020-01-09 17:10:48 +0100

    add traversal of a BST

--- BinarySearchTrees/BSTTraversal/python/program.py
+++ BinarySearchTrees/BSTTraversal/python/program.py
@@ -1,27 +1,38 @@
+# implementaion of the 2 different Order Traverse
+
+
 def inOrderTraverse(tree, array):
-    # Write your code here.
-	if tree == None:
-		return array
-		
-	array = inOrderTraverse(tree.left, array)
-	array.append(tree.value)
-	array = inOrderTraverse(tree.right, array)
-	return array
-def preOrderTraverse(tree, array):
-    # Write your code here.
-	if tree == None:
-		return array
+    """
+    """
+    if tree is None:
+        return array
+
+    array = inOrderTraverse(tree.left, array)
     array.append(tree.value)
-	array = preOrderTraverse(tree.left, array)
-	array = preOrderTraverse(tree.right, array)
-	return array
+    array = inOrderTraverse(tree.right, array)
+    return array
+
+
+def preOrderTraverse(tree, array):
+    """
+
+    """
+    if tree is None:
+        return array
+
+    array.append(tree.value)
+    array = preOrderTraverse(tree.left, array)
+    array = preOrderTraverse(tree.right, array)
+    return array
 
 
 def postOrderTraverse(tree, array):
-	if tree == None:
-		return array
-    # Write your code here.
+    """
+
+    """
+    if tree is None:
+        return array
     array = postOrderTraverse(tree.left, array)
-	array = postOrderTraverse(tree.right, array)
-	array.append(tree.value)
-	return array
+    array = postOrderTraverse(tree.right, array)
+    array.append(tree.value)
+    return array
