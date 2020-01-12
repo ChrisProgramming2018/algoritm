1752ab1ff141a2a0eeed7705d0ec3761be89e9c8
Christian Leininger <info2016frei@gmail.com>
2020-01-12 11:28:41 +0100

    add branchsums start

---
+++ BinaryTrees/BranchSums/python/program.py
@@ -0,0 +1,45 @@
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+
+
+def branchSums(tree):
+    """
+
+    Args:
+        param1: (class BinaryTree)
+    Return:
+    
+    """
+    return rekursiveSums(tree, [], 0)
+
+
+
+def rekursiveSums(tree, sums, value):
+    """
+    Traverse the tree in preorder root left right
+    Args:
+        param1:(class BinaryTree)
+        param2:(list) sums 
+        param3:(int) value
+
+    """
+
+    # case the called does not exist
+    if tree is None:
+        return
+
+    # case leave node (has no children
+    new_value = value + tree.value
+    if tree.left is None and tree.right is None:
+        sums.append(new_value)
+
+    # travers to the tree in preorder 
+    rekursiveSums(tree.left, sums, new_value)
+    rekursiveSums(tree.right, sums, new_value)
+
