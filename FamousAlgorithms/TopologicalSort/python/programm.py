fed5d92402c0ff2f93dc6464063b118e1f06da77
Christian Leininger <info2016frei@gmail.com>
2020-01-15 09:21:40 +0100

    add graph for topoogical Sort

---
+++ FamousAlgorithms/TopologicalSort/python/programm.py
@@ -0,0 +1,31 @@
+# Implementation of Topological Sort
+
+
+class JobNode:
+    def __init__(self, job):
+        self.job = job
+        self.prereqs = []
+        self.visited = False
+        self.visiting = False
+
+
+class JobGraph:
+    def __init__(self, jobs):
+        self.nodes = []
+        self.graph = {}
+        for job in jobs:
+            self.addNode(job)
+
+    def addNode(self, job):
+        self.graph[job] = JobNode(job)
+        self.nodes.append(self.graph[job])
+
+    def getNode(self, job):
+        if job not in self.graph:
+            self.addNode(job)
+        return self.graph[job]
+
+    def addPrereq(self, job,  prereq):
+        jobNode = self.getNode(job)
+        prereqNode = self.getNode(prereq)
+        jobNode.prereqs.append(prereqNode)
