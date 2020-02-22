cbb8e377222c55372ed347cd2603ca81d41c80e0
Christian <christianleininger42@gmail.com>
2020-02-22 19:12:50 +0100

    add part master sol

--- DynamicProgramming/MaxSumIncreasingSubsequenece/python/program.py
+++ DynamicProgramming/MaxSumIncreasingSubsequenece/python/program.py
@@ -2,6 +2,36 @@
 
 
 
+def masterSol(array):
+    sequences = [None for _ in array]
+    sums = [num for num in array]
+    maxSumIdx = 0
+    for i in range(len(array)):
+        currentNum = array[i]
+        for j in range(0,i):
+            otherNum = array[j]
+            if otherNum < currentNum and sums[j] + currentNum >= sums[i]:
+                sums[i] = sums[j] + currentNum
+                sequences[i] = j
+
+        if sums[i] >= sums[maxSumIdx]:
+            maxSumIdx = i
+    return [sums[maxSumIdx], buildSequence(array, sequences, maxSumIdx)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def maxSumIncreasingSubsequence(array):
     # Write your code here.
     length = len(array)
