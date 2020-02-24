8d595a6c64b2ce111a77e88aa64ec5acc9a44f34
Christian Leininger <info2016frei@gmail.com>
2020-02-24 16:26:44 +0100

    add three number sum that pass test

---
+++ Arrays/ThreeNumberSum/python/program.py
@@ -0,0 +1,32 @@
+
+def threeNumberSum(array, target): 
+    array.sort()
+    solution = []
+    current_pos = 0
+    lpointer = 1
+    rpointer = len(array) - 1
+    # only go until the 3 last position 
+    while current_pos <= len(array) - 3:
+        while True:
+            if lpointer >= rpointer:
+                current_pos +=1
+                #current_pos = 100
+                lpointer = current_pos + 1
+                rpointer = len(array) -1
+                break
+                
+            res = array[current_pos] + array[rpointer] + array[lpointer] 
+            #print("sol  is {} = {} + {} + {}".format(res, array[current_pos], array[rpointer], array[lpointer]))
+            # 3 cases
+            if res > target:
+                # res needs to get lower
+                rpointer -=1
+
+            if res < target:
+                # res needs to get higher
+                lpointer +=1
+            if res == target:
+                solution.append([array[current_pos], array[lpointer], array[rpointer]])
+                lpointer +=1
+                rpointer -=1
+    return solution
