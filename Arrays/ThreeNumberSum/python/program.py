0aaf3e33aafe3a77fb2bde425cd5a9e8adc87f06
Christian Leininger <info2016frei@gmail.com>
2020-02-24 16:29:30 +0100

    add three number sum checkstyle

--- Arrays/ThreeNumberSum/python/program.py
+++ Arrays/ThreeNumberSum/python/program.py
@@ -1,32 +1,31 @@
 
-def threeNumberSum(array, target): 
+def threeNumberSum(array, target):
     array.sort()
     solution = []
     current_pos = 0
     lpointer = 1
     rpointer = len(array) - 1
-    # only go until the 3 last position 
+    # only go until the 3 last position
     while current_pos <= len(array) - 3:
         while True:
             if lpointer >= rpointer:
-                current_pos +=1
-                #current_pos = 100
+                current_pos += 1
                 lpointer = current_pos + 1
-                rpointer = len(array) -1
+                rpointer = len(array) - 1
                 break
-                
-            res = array[current_pos] + array[rpointer] + array[lpointer] 
-            #print("sol  is {} = {} + {} + {}".format(res, array[current_pos], array[rpointer], array[lpointer]))
+
+            res = array[current_pos] + array[rpointer] + array[lpointer]
             # 3 cases
             if res > target:
                 # res needs to get lower
-                rpointer -=1
+                rpointer -= 1
 
             if res < target:
                 # res needs to get higher
-                lpointer +=1
+                lpointer += 1
+                
             if res == target:
                 solution.append([array[current_pos], array[lpointer], array[rpointer]])
-                lpointer +=1
-                rpointer -=1
+                lpointer += 1
+                rpointer -= 1
     return solution
