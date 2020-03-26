eb85f9b7c55f144123ad72df6381b77580333c74
Christian <christianleininger42@gmail.com>
2020-03-26 13:48:35 +0100

    add trival sol for permutations

--- Recursion/Permutations/python/permutations.py
+++ Recursion/Permutations/python/permutations.py
@@ -1,15 +1,31 @@
+import random
+import copy
+
+
+
+
+
+def facult(n):
+    res = 1
+    for i in range(1,n+1):
+        res *=i
+    return res
 
 
 def permuataions_trival(array):
     res = []
     length = facult(len(array))
+    orginal = copy.deepcopy(array)
     while len(res) <= length:
+        array = copy.deepcopy(orginal)
         tmp = []
         while True:
             item = random.choice(array)
-            tmp.apppend(item)
+            tmp.append(item)
             array.remove(item)
             if len(array) == 0:
+                if tmp is not res:
+                    res.append(tmp)
                 break
 
     return res
@@ -18,7 +34,6 @@ def permuataions_trival(array):
 
 
 
-
-def __name__ == "__main__":
-    array = [1,2,3,4]
-    print(permuataions_trival)
+if __name__ == "__main__":
+    array = [1,2,3,4,5]
+    print(permuataions_trival(array))
