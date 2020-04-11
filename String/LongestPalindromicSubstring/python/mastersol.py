dd244c031f342c617e0f8308dce271e29f00c29b
Christian Leininger <info2016frei@gmail.com>
2020-04-11 19:06:38 +0200

    add mastersol

---
+++ String/LongestPalindromicSubstring/python/mastersol.py
@@ -0,0 +1,45 @@
+
+# O(n^3) time | O(1) space
+def longestPalindromicSubstring(string):
+    longest = ""
+    for i in range(len(string)):
+        for j in range(i, len(string)):
+            substring = string[i:j +1]
+            if len(substring) > len(longest) and isPalindrome(substring):
+                longest = substring
+    return longest
+
+
+
+def isPalindrome(string):
+    leftIdx = 0
+    rightIdx = len(string) - 1
+    while leftIdx < rightIdx:
+        if string[leftIdx] != string[rightIdx]:
+            return False
+
+        leftIdx += 1
+        rightIdx -= 1
+
+    return True
+
+#O(n^2) time | O(1) space 
+def longestPalindromicSubstring(string):
+    currentLongest = [0,1]
+    for i in range(1, len(string)):
+        odd = getLongestPalindromeFrom(string, i - 1, i + 1)
+        even = getLongestPalindromeFrom(string, i - 1, i)
+        longest = max(odd, even, key= lambda x:x[1] - x[0])
+        currentLongest = max(longest, currentLongest, key=lambda x:x[1] - x[0])
+
+    return string[currentLongest[0] : currentLongest[1]]
+
+def getLongestPalindromeFrom(string, leftIdx, rightIdx):
+    while leftIdx >= 0 and rightIdx < len(string):
+        if string[leftIdx] != string[rightIdx]:
+            break
+
+        leftIdx -= 1
+        rightIdx += 1
+    return [leftIdx + 1, rightIdx]
+            
