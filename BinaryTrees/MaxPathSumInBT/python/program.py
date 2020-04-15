8b539d981095d5f897fb47b6cee74e9847d57fbb
Christian Leininger <info2016frei@gmail.com>
2020-04-15 10:32:27 +0200

    add docstring for maxPath

---
+++ BinaryTrees/MaxPathSumInBT/python/program.py
@@ -0,0 +1,17 @@
+
+
+def maxPathSum(tree):
+    """
+    Path is a collection of nodes which each is not more then 2 other nodes are
+    connected
+
+
+    Args:
+        param1(BT): binary tree 
+
+    Return: int represents sum of max path
+
+
+    """
+    res = 0
+    return res 
