fc9144df34518d05b88fba0c58fce90882fe84a4
Christian Leininger <info2016frei@gmail.com>
2020-04-15 13:26:47 +0200

    add sol cpp passes test

--- Arrays/TwoNumberSum/cpp/solution.cpp
+++ Arrays/TwoNumberSum/cpp/solution.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <unordered_set>
-#include <algoritm>
+#include <algorithm>
+#include <iostream>
 
 
 
@@ -8,7 +9,7 @@
 std::vector<int> twoNumberSum1(std::vector<int> array , int targetSum) {
   for (int i = 0; i < array.size() - 1; i++) {
     int firstNum = array[i];
-    for (int j = 0; j < array.size() - 1; j++) {
+    for (int j = i + 1; j < array.size(); j++) {
       int secondNum = array[j];
       if (firstNum + secondNum == targetSum) {
         return std::vector<int> {firstNum, secondNum};
@@ -21,21 +22,47 @@ std::vector<int> twoNumberSum1(std::vector<int> array , int targetSum) {
 // O(n²) time | O(1) space
 std::vector<int> twoNumberSum2(std::vector<int> array , int targetSum) {
   std::unordered_set<int> nums;
-  for (int num :array) {
+  for (int num : array) {
     int potentialMatch = targetSum - num;
     if (nums.find(potentialMatch) !=nums.end()) {
       return std::vector<int>{potentialMatch, num};
     } else {
-      nums.inser(num);
+      nums.insert(num);
     }
   }
   return {};
 }
 
 
-// O(n²) time | O(1) space
-std::vector<int> twoNumberSum2(std::vector<int> array , int targetSum) {
+// O(n log n) time | O(1) space
+std::vector<int> twoNumberSum3(std::vector<int> array , int targetSum) {
   std::sort(array.begin(), array.end());
   int left  = 0;
   int right = array.size() - 1;
+  while (left < right) {
+    int currentSum = array[left] + array[right];
+    if (currentSum = targetSum) { 
+      // since only solution is possible
+      return {array[left], array[right]};
+    } else if (currentSum < targetSum) {
+      // currentSum need to be higher 
+      // since it is sorted move left pointer increase the sum
+      left++;
+    } else {
+      right--;
+    }
+  }
+  // no pair found
+  return {};
+}
 
+
+int main() {
+  std::vector<int> sol = twoNumberSum1({4, 6, 1, -3}, 3);
+  std::cout << "sol 1: [" << sol[0] << ", " << sol[1] << "] == [-3, 6]" << std::endl;
+  sol = twoNumberSum2({4, 6, 1, -3}, 3);
+  std::cout << "sol 2: [" << sol[0] << ", " << sol[1] << "] == [-3, 6]" << std::endl;
+  sol = twoNumberSum3({4, 6, 1, -3}, 3);
+  std::cout << "sol 3: [" << sol[0] << ", " << sol[1] << "] == [-3, 6]" << std::endl;
+  return 0;
+}
