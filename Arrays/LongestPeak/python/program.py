00b79b9067db7d4a2105cef605b772785329e6d4
Christian Leininger <info2016frei@gmail.com>
2020-04-15 10:47:34 +0200

    add start code longest peak

---
+++ Arrays/LongestPeak/python/program.py
@@ -0,0 +1,19 @@
+
+
+
+def longestPeak(array):
+    """
+
+
+    Args:
+        param1(list): array with ints
+    
+    Return: int represent length of longest peak
+
+    """
+    # all array with less then 4 its zero
+    if len(array) < 4:
+        return 0
+
+    res = 0
+    return res
