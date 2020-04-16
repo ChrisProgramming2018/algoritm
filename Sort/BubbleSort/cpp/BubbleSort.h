122992d45d4b3f8663b99ed248dff7459c005798
Christian Leininger <info2016frei@gmail.com>
2020-04-16 10:17:14 +0200

    add h file bubblsort

---
+++ Sort/BubbleSort/cpp/BubbleSort.h
@@ -0,0 +1,15 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <iostream>
+#include <vector>
+
+
+#ifndef SORT_BUBBLESORT_CPP_BUBBLESORT_H_
+#define SORT_BUBBLESORT_CPP_BUBBLESORT_H_
+
+std::vector<int> BubbleSort(std::vector<int> array);
+#endif // SORT_BUBBLESORT_CPP_BUBBLESORT_H_
+
