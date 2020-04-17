3544a096cd1b8bebac5cf7448f67ba293841300a
Christian Leininger <info2016frei@gmail.com>
2020-04-17 10:47:18 +0200

    add begin progam

---
+++ LinkedLists/RemoveKthNodeFromEnd/python/program.py
@@ -0,0 +1,19 @@
+# Copyright 2020
+# Author: Christian Leininger <info2016frei@gmail.com>
+
+
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeKthNodeFromEnd(head, k):
+    """ Assumes linked list has at least k nodes
+
+    Args:
+
+    Return:
+    """
+    pass
