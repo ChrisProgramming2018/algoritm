76a21a88af33b30ed57371acb8219c7e47649dfb
Christian Leininger <info2016frei@gmail.com>
2020-04-17 12:46:37 +0200

    add code passes all tests

--- LinkedLists/RemoveKthNodeFromEnd/python/program.py
+++ LinkedLists/RemoveKthNodeFromEnd/python/program.py
@@ -11,9 +11,33 @@ class LinkedList:
 
 def removeKthNodeFromEnd(head, k):
     """ Assumes linked list has at least k nodes
-
+    will remove the k element from the end in place
     Args:
+        param1(singleLinkedList): head
+        param2(int): k
 
     Return:
     """
-    pass
+    pos1 = 0
+    length = 0
+    node = head
+    while True:
+        if node is None:
+            break
+        length += 1
+        node = node.next
+
+    delete = length - k
+    length = 0
+    # case is first element
+    if delete == 0:
+        head.value = head.next.value
+        head.next = head.next.next
+        return
+    node = head
+    while True:
+        if length == delete - 1:
+            node.next = node.next.next
+            break
+        length += 1
+        node = node.next
