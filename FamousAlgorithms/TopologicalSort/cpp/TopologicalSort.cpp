1030ce0b19d2561c24805bf2e97bb010b533ebde
Christian Leininger <info2016frei@gmail.com>
2020-04-18 07:54:37 +0200

    start

---
+++ FamousAlgorithms/TopologicalSort/cpp/TopologicalSort.cpp
@@ -0,0 +1,10 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+#include "./TopologicalSort.h"
+
+// ______________________________________________________________
+JobGraph* JobGraph::createGraphNode(std::vector<int> jobs, std::vector<std::vector<int>> deps) {
+}
+
