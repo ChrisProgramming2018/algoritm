8ebe6776374af76aab208ccc71b554276de59d55
Christian Leininger <info2016frei@gmail.com>
2020-04-19 19:22:10 +0200

    add code passes test

---
+++ Sort/InsertionSort/cpp/InsertionSort.h
@@ -0,0 +1,16 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+void swap(std::vector<int> &array, int left, int right);
+std::vector<int> insertionSort(std::vector<int> array);
+
+#endif //
+
