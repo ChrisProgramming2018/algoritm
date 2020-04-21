effbc5e0764a6975734111114ef109a82d772e02
Christian Leininger <info2016frei@gmail.com>
2020-04-21 16:27:30 +0200

    add code

---
+++ BinaryTrees/BranchSums/cpp/BranchSum.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+#include "./BrunchSum.h"
+
+
+// ________________________________________________________________________________________________
+std::vector<int> branchSums(BinaryTree *root) {
+  std::vector<int> sums;
+  calculateBranchSums(root, 0, sums);
+  return sums;
+}
+
+
+// ________________________________________________________________________________________________
+void calculateBranchSums(BinaryTree *node, int runningSum, std::vector<int> &sums) {
+  if (node == NULL) return;
+  int newRunningSum = runningSum + node->value;
+  if (node->left == NULL && node->right == NULL) {
+    sums.push_back(newRunningSum);
+    return;
+  }
+
+  calculateBranchSums(node->left, newRunningSum, sums);
+  calculateBranchSums(node->right, newRunningSum, sums);
+}
