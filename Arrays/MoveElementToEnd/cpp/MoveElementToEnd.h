002c08b8ab0819645d03596e9884927ee8490482
Christian Leininger <info2016frei@gmail.com>
2020-04-21 22:29:05 +0200

    add code passes test

---
+++ Arrays/MoveElementToEnd/cpp/MoveElementToEnd.h
@@ -0,0 +1,15 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+#include <algorithm>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+std::vector<int> moveElementToEnd(std::vector<int> array, int toMove);
+#endif //
+
