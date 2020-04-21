a12ee1e9b1d6c1f4b1e93ee994e9403a24f44ff3
Christian Leininger <info2016frei@gmail.com>
2020-04-21 20:09:22 +0200

    add code passes  test

---
+++ Arrays/TwoNumberSum/cpp/TwoNumberSum.h
@@ -0,0 +1,20 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <iostream>
+#include <vector>
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+
+std::vector<int> twoNumberSum(std::vector<int> array , int targetSum);
+std::vector<int> twoNumberSum2(std::vector<int> array , int targetSum);
+std::vector<int> twoNumberSum3(std::vector<int> array , int targetSum);
+
+
+
+#endif //
+
