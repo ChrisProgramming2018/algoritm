373b1ade6e7fcf4de407df3053b58465e268c3f1
Christian Leininger <info2016frei@gmail.com>
2020-04-21 20:46:14 +0200

    add code passes test

---
+++ Arrays/ThreeNumberSum/cpp/ThreeNumberSum.h
@@ -0,0 +1,14 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+#include <algorithm>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+std::vector<std::vector<int>> threeNumberSum(std::vector<int> array, int targetSum);
+#endif //
