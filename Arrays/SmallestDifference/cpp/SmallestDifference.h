28c7b4e1b908607a857f980320b143df46601cc7
Christian Leininger <info2016frei@gmail.com>
2020-04-21 21:15:42 +0200

    add code passes test

---
+++ Arrays/SmallestDifference/cpp/SmallestDifference.h
@@ -0,0 +1,16 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+std::vector<int> smallestDifference(std::vector<int> arrayOne, std::vector<int> arrayTwo);
+#endif //
+
