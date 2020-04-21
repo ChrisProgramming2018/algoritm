018924f8f121f21c131e8fb121a01bfa702e5983
Christian Leininger <info2016frei@gmail.com>
2020-04-21 20:09:43 +0200

    add code test

--- Arrays/TwoNumberSum/cpp/TwoNumberSumTest.cpp
+++ Arrays/TwoNumberSum/cpp/TwoNumberSumTest.cpp
@@ -6,63 +6,37 @@
 
 
 #include <gtest/gtest.h>
-#include "TwoNumberSumMain.cpp"
+#include "TwoNumberSum.h"
 
 
-
-  
+TEST(TwoNumberSumTest, TwoNumberSum)   {
+ std::vector<int> expected {4, 6};
+ ASSERT_EQ(twoNumberSum({4, 6}, 10), expected);
  
-/*  
-  TEST_START;
-  TEST_CASE("Test Case 1") {
-    vector<int> expected {4, 6};
-    REQUIRE(twoNumberSum({4, 6}, 10) == expected);
-  }
+ expected = {1, 4};
+ ASSERT_EQ(twoNumberSum({4, 6, 1}, 5), expected);
+ 
+ expected = {-3, 6};
+ ASSERT_EQ(twoNumberSum({4, 6, 1, -3}, 3), expected);
+ 
+ expected =  {-1, 11};
+ ASSERT_EQ(twoNumberSum({3, 5, -4, 8, 11, 1, -1, 6}, 10), expected);
+ 
+ expected = {8, 9};
+ ASSERT_EQ(twoNumberSum({1, 2, 3, 4, 5, 6, 7, 8, 9}, 17), expected);
+ 
+ expected = {3, 15};
+ ASSERT_EQ(twoNumberSum({1, 2, 3, 4, 5, 6, 7, 8, 9, 15}, 18), expected);
+ 
+ expected = {-5, 0};
+ ASSERT_EQ(twoNumberSum({-7, -5, -3, -1, 0, 1, 3, 5, 7}, -5), expected);
 
-  TEST_CASE("Test Case 2") {
-    vector<int> expected {1, 4};
-    REQUIRE(twoNumberSum({4, 6, 1}, 5) == expected);
-  }
-
-  TEST_CASE("Test Case 3") {
-    vector<int> expected {-3, 6};
-    REQUIRE(twoNumberSum({4, 6, 1, -3}, 3) == expected);
-  }
-
-  TEST_CASE("Test Case 4") {
-    vector<int> expected {-1, 11};
-    REQUIRE(twoNumberSum({3, 5, -4, 8, 11, 1, -1, 6}, 10) == expected);
-  }
-
-  TEST_CASE("Test Case 5") {
-    vector<int> expected {8, 9};
-    REQUIRE(twoNumberSum({1, 2, 3, 4, 5, 6, 7, 8, 9}, 17) == expected);
-  }
-
-  TEST_CASE("Test Case 6") {
-    vector<int> expected {3, 15};
-    REQUIRE(twoNumberSum({1, 2, 3, 4, 5, 6, 7, 8, 9, 15}, 18) == expected);
-  }
-
-  TEST_CASE("Test Case 7") {
-    vector<int> expected {-5, 0};
-    REQUIRE(twoNumberSum({-7, -5, -3, -1, 0, 1, 3, 5, 7}, -5) == expected);
-  }
-
-  TEST_CASE("Test Case 8") {
-    vector<int> expected {-47, 210};
-    REQUIRE(twoNumberSum({-21, 301, 12, 4, 65, 56, 210, 356, 9, -47}, 163) == expected);
-  }
-
-  TEST_CASE("Test Case 9") {
-    vector<int> expected {};
-    REQUIRE(twoNumberSum({-21, 301, 12, 4, 65, 56, 210, 356, 9, -47}, 164) == expected);
-  }
-
-  TEST_CASE("Test Case 10") {
-    vector<int> expected {};
-    REQUIRE(twoNumberSum({3, 5, -4, 8, 11, 1, -1, 6}, 15) == expected);
-  }
-  TEST_END;
+ expected = {-47, 210};
+ ASSERT_EQ(twoNumberSum({-21, 301, 12, 4, 65, 56, 210, 356, 9, -47}, 163), expected);
+ 
+ expected = {};
+ ASSERT_EQ(twoNumberSum({-21, 301, 12, 4, 65, 56, 210, 356, 9, -47}, 164), expected);
+ 
+ expected =  {};
+ ASSERT_EQ(twoNumberSum({3, 5, -4, 8, 11, 1, -1, 6}, 15),expected);
 }
-*/
