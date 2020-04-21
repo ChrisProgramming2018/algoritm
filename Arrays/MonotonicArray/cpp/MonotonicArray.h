8521f330cf798fb353b67b2ae4db3245c02614f9
Christian Leininger <info2016frei@gmail.com>
2020-04-21 22:58:33 +0200

    add code passes test

---
+++ Arrays/MonotonicArray/cpp/MonotonicArray.h
@@ -0,0 +1,14 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+bool isMonotonic(std::vector<int> array);
+#endif //
+
