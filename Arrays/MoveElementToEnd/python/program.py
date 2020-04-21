a9a17582e367668208050ba99285abf0384dc08c
Christian Leininger <info2016frei@gmail.com>
2020-04-21 22:11:37 +0200

    add code passes test

--- Arrays/MoveElementToEnd/python/program.py
+++ Arrays/MoveElementToEnd/python/program.py
@@ -1,6 +1,20 @@
 
 
 
+def mastersol(array, toMove):
+    i = 0
+    j = len(array) - 1
+    while i < j:
+        while i < j and array[j] == toMove:
+            j -= 1
+        if array[i] == toMove:
+            array[i], array[j] = array[j], array[i]
+        i += 1
+
+    return array
+
+
+
 #time O(n) | space O(1)
 def moveElementToEnd(array, toMove):
     """ Moves the toMove int to the end of the list
