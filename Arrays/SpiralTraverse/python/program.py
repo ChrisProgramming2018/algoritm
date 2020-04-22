0e22cce6fad6c7c187167ab126e8239ec084f832
Christian Leininger <info2016frei@gmail.com>
2020-04-22 08:32:02 +0200

    add sol

--- Arrays/SpiralTraverse/python/program.py
+++ Arrays/SpiralTraverse/python/program.py
@@ -1,5 +1,35 @@
 
 
+def masterSol(array):
+    result = [] 
+    spiralFill(array, 0, len(array) - 1, 0, len(array[0]) - 1, result)
+    return result
+
+
+def spiralFill(array, startRow, endRow, startCol, endCol, result):
+    if startRow > endRow or startCol > endCol:
+        return
+
+    for col in range(startCol, endCol + 1):
+        result.append(array[startRow][col])
+
+    for row in range(startRow + 1, endRow + 1):
+        result.append(array[row][endCol])
+
+    for col in reversed(range(startCol, endCol)):
+        if startRow == endRow:
+            break
+        result.append(array[endRow][col])
+
+    for row in reversed(range(startRow + 1, endRow)):
+        if startCol == endCol:
+            break
+        result.append(array[row][startCol])
+
+    spiralFill(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result)
+ 
+
+
 
 
 
