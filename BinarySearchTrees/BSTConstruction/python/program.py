4c0557461152cd7005d4327be5b1890e299d90c8
Christian Leininger <info2016frei@gmail.com>
2020-04-22 14:19:40 +0200

    add code passes test

--- BinarySearchTrees/BSTConstruction/python/program.py
+++ BinarySearchTrees/BSTConstruction/python/program.py
@@ -13,28 +13,63 @@ class BST:
                 if current_node.left is None:
                     current_node.left = BST(value)
                     break
+                else:
+                    current_node = current_node.left
             else:
                 if current_node.right is None:
-                    current_node = BST(value)
+                    current_node.right = BST(value)
                     break
                 else:
                     current_node = current_node.right 
         return self
 
-     def contains(self, value):
-        # Write your code here.
-        current = self
-        while True:
-            if current.value == value:
-                return True
-            if value >= current.value:
-                current = current.right
+    def contains(self, value):
+        current_node = self
+        while current_node is not None:
+            if value < current_node.value:
+                current_node = current_node.left
+            elif value > current_node.value:
+                current_node = current_node.right
             else:
-                current = current.left
-            if current is None:
-                return False
+                return True
+        return False
     
-    def remove(self, value):
-        # Write your code here.
-        # Do not edit the return statement of this method.
+    def remove(self, value, parent_node=None):
+        current_node = self
+        while current_node is not None:
+            if value < current_node.value:
+                parent_node = current_node
+                current_node = current_node.left
+            elif value > current_node.value:
+                parent_node = current_node
+                current_node = current_node.right
+            else:
+                if current_node.left is not None and current_node.right is not None:
+                    current_node.value = current_node.right.getMinVAlue()
+                    current_node.right.remove(current_node.value, current_node)
+                elif parent_node is None:
+                    if current_node.left is not None:
+                        current_node.value = current_node.left.value
+                        current_node.right = current_node.left.right
+                        current_node.left = current_node.left.left
+                    elif current_node.right is not None:
+                        current_node.value = current_node.right.value
+                        current_node.left = current_node.right.left
+                        current_node.right = current_node.right.right
+                    else:
+                        pass
+                elif parent_node.left == current_node:
+                    parent_node.left = current_node.left if current_node.left is not None else current_node.right
+
+                elif parent_node.right == current_node:
+                    parent_node.right = current_node.left if current_node.left is not None else current_node.right
+                break
         return self
+
+    def getMinVAlue(self):
+        current_node = self
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+                    
+
