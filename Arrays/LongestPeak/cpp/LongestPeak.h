b8a274969aae1b73e44f6b9f01c04208ff371fc6
Christian Leininger <info2016frei@gmail.com>
2020-04-22 10:41:14 +0200

    code passes test

---
+++ Arrays/LongestPeak/cpp/LongestPeak.h
@@ -0,0 +1,14 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+#include <algorithm>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+int longestPeak(std::vector<int> array);
+#endif //
