6bad579b5c451d527ccd5cf8d82573d5fd22828d
Christian Leininger <info2016frei@gmail.com>
2020-04-23 08:54:51 +0200

    add code passes test

---
+++ DynamicProgramming/MinNumberOfCoinsForChange/cpp/MinNumberOfCoinsForChange.h
@@ -0,0 +1,15 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+#include <climits>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+int minNumberOfCoinsForChange(int n, std::vector<int> denoms);
+#endif //
+
