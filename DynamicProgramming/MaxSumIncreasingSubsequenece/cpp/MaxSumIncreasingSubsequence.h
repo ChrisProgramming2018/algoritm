a37d8d56aa76ab2659d502d8827b31ba81ec9fb7
Christian Leininger <info2016frei@gmail.com>
2020-04-23 10:06:03 +0200

    code

---
+++ DynamicProgramming/MaxSumIncreasingSubsequenece/cpp/MaxSumIncreasingSubsequence.h
@@ -0,0 +1,16 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+#include <climits>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+std::vector<vector<int>> maxSumIncreasingSubsequence(vector<int> array);
+std::vector<std::vector<int>> buildSequence(std::vector<int> array, std::vector<int> sequences, int currentIdx, int sum);
+#endif //
+
