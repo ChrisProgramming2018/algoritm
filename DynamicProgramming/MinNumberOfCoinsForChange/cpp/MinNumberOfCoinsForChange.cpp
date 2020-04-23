5b5a2f6e95bb08305fd6b0b7c7458fa3294a2cab
Christian Leininger <info2016frei@gmail.com>
2020-04-23 08:54:32 +0200

    add code passes test

---
+++ DynamicProgramming/MinNumberOfCoinsForChange/cpp/MinNumberOfCoinsForChange.cpp
@@ -0,0 +1,26 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include "./MinNumberOfCoinsForChange.h"
+
+// ______________________________________________________________
+int minNumberOfCoinsForChange(int n, std::vector<int> denoms ){
+  std::vector<int> numOfCoins(n + 1, INT_MAX);
+  numOfCoins[0] = 0;
+  int toCompare = 0;
+  for (int denom : denoms) {
+    for (int amount = 0; amount < numOfCoins.size(); amount++) {
+      if (denom <= amount) {
+        if (numOfCoins[amount - denom] == INT_MAX) {
+          toCompare = numOfCoins[amount - denom];
+        } else { 
+          toCompare = numOfCoins[amount - denom] + 1;
+        }
+        numOfCoins[amount] = std::min(numOfCoins[amount], toCompare);
+      }
+    }
+  }
+  return numOfCoins[n] != INT_MAX ? numOfCoins[n] : -1;
+}
