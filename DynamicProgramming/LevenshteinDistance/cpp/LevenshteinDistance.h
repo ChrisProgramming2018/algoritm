6ec669ed7061c78a81df9cccbd4520c7f395f03b
Christian Leininger <info2016frei@gmail.com>
2020-04-23 09:12:26 +0200

    add code passes tests

---
+++ DynamicProgramming/LevenshteinDistance/cpp/LevenshteinDistance.h
@@ -0,0 +1,14 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+int LevenshteinDistance(std::string str1, std::string str2);
+
+#endif //
