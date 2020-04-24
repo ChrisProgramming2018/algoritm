af2a18fe65505e261e370621e42192665baed493
Christian Leininger <info2016frei@gmail.com>
2020-04-24 11:45:07 +0200

    add code passes test

--- Tries/SuffixTrieConstruction/python/program.py
+++ Tries/SuffixTrieConstruction/python/program.py
@@ -9,7 +9,33 @@ class SuffixTrie:
         self.populateSuffixTrieFrom(string)
         
     def populateSuffixTrieFrom(self, string):
-        pass
+        """
+
+        """
+        for idx in range(len(string)):
+            self.insertSubstringStartingAt(idx, string)
     
     def contains(self, string):
-        pass
+        """
+        run time O(n) | space O(1)
+        """
+        node = self.root
+        for letter in string:
+            if letter  not in node:
+                return False
+            node = node[letter]
+        return self.endSymbol in node
+
+
+    def insertSubstringStartingAt(self, idx, string):
+        """
+        
+        """
+        node = self.root
+        for j in range(idx, len(string)):
+            letter = string[j]
+            if letter not in node:
+                node[letter] = {}
+            node = node[letter]
+        node[self.endSymbol] = True
+
