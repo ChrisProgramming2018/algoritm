27fe6d8562393e9e37947568823c3aae4a74ca1e
Christian Leininger <info2016frei@gmail.com>
2020-04-26 16:42:16 +0200

    add first code

---
+++ DynamicProgramming/DiskStacking/python/Diskstacking.py
@@ -0,0 +1,19 @@
+# Copyright 2020
+# Author: Christian Leininger <info2016frei@gmail.com>
+
+
+
+def diskStacking(disks): 
+    """
+
+    Args:
+        param1(listOflist):
+
+    Return List of List
+    """
+    if len(disks) < 2:
+        return disks
+
+
+
+                                                    
