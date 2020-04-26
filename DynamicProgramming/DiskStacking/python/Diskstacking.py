0f1f9ff18c77197c0677704cff1db3b7f91c58ca
Christian Leininger <info2016frei@gmail.com>
2020-04-26 16:47:44 +0200

    add first code

--- DynamicProgramming/DiskStacking/python/Diskstacking.py
+++ DynamicProgramming/DiskStacking/python/Diskstacking.py
@@ -16,4 +16,18 @@ def diskStacking(disks):
 
 
 
-                                                    
+
+
+def validDisk(disk_c, disk_old):
+    """
+    Compares the to disk if the disk_c has strict higher value 
+    then disk_old the function returns true
+
+    Args:
+         param1:(list) disk 3 elements w, d, h
+
+    Return bool
+    
+    """
+
+    return disk_old[0] < disk_c[0] and disk_old[1] < disk_c[01 and disk_old[2] < disk_c[2] 
