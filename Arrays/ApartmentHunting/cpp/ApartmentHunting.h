ee92d7ed584ea765e8e4cc756a4cc15dffb67ef1
Christian Leininger <info2016frei@gmail.com>
2020-04-28 12:17:52 +0200

    add h file

---
+++ Arrays/ApartmentHunting/cpp/ApartmentHunting.h
@@ -0,0 +1,21 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+#include <unordered_map>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <string>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+int aparmentHunting(std::vector<unordered_map<std::string, bool> blocks, std::vector<std::string reqs);
+int getIdxAtMinValue(std::vector<int> array);
+int distanceBetween(int a, int b);
+#endif //
+
