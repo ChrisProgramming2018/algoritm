aca91757ef062fb22c3301f3f07a05acbab7e07f
Christian Leininger <info2016frei@gmail.com>
2020-04-29 12:37:31 +0200

    add code

--- FamousAlgorithms/TopologicalSort/cpp/TopologicalSort.cpp
+++ FamousAlgorithms/TopologicalSort/cpp/TopologicalSort.cpp
@@ -6,19 +6,55 @@
 
 // ______________________________________________________________
 std::vector<int> topologicalSort(std::vector<int> jobs, std::vector<std::vector<int>> deps) {
-  JobGraph *jobGraph = (jobs, deps);
-  
-
-
-
+  JobGraph *jobGraph = createGraphNode(jobs, deps);
+  return getOrderedJobs(jobGraph);
 }
+
+// ______________________________________________________________
+std::vector<int> getOrderedJobs(JobGraph *graph) {
+  std::vector<int> orderedJobs = {};
+
+
 
 
 
 // ______________________________________________________________
 JobGraph* createGraphNode(std::vector<int> jobs, std::vector<std::vector<int>> deps) {
-
-
+  JobGraph *graph = new JobGraph(jobs);
+  for (std::vector<int> dep : deps) {
+    graph->addPreqeq(dep[1], dep[0]);
+  }
+  return graph;
 }
 
 
+// ______________________________________________________________
+bool depthFirstTraverse(JobGraph *graph) {
+
+
+  return false;
+}
+
+
+// ______________________________________________________________
+JobGraph::JobGraph(std::vector<int> jobs) {
+  _nodes = {};
+  for (int job : jobs) {
+    this->addNode(job);
+  }
+}
+// ______________________________________________________________
+void JobGraph::addPreqeq(int job, int preqeq) {
+  JobNode *jobNode = getNode(job);
+  JobNode *preqeqNode = getNode(preqeq);
+  jobNode->_preqeqs.push_back(preqeqNode);
+}
+
+
+
+
+// ______________________________________________________________
+
+// ______________________________________________________________
+
+// ______________________________________________________________
