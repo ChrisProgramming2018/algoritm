286e89842847acb1e0697c3e2707638736eaa27d
Christian Leininger <info2016frei@gmail.com>
2020-04-29 10:00:53 +0200

    add code

---
+++ DynamicProgramming/LongestCommonSubsequence/cpp/LongestCommonSubSequence.h
@@ -0,0 +1,19 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+#include <string>
+
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+
+std::vector<char> buildSequence(std::vector<std::vector<int>> lengths, std::string str);
+std::vector<char> longestCommonSubSequence(std::string str1, std::string str2);
+
+
+#endif //
+
