5c2fc43e658ff7a704fc0d5ef3674c1bc81eca40
Christian Leininger <info2016frei@gmail.com>
2020-04-29 06:33:42 +0200

    add func graph

--- FamousAlgorithms/TopologicalSort/cpp/TopologicalSort.h
+++ FamousAlgorithms/TopologicalSort/cpp/TopologicalSort.h
@@ -24,6 +24,13 @@ class JobNode {
 class JobGraph {
  public:
   std::vector<JobNode*> nodes;
+  JobGraph(std::vector<int> jobs);
+  void addPreqeq(int job, int preqeq);
+  void addNode(int job);
+  JobNode* getNode
 
+};
+  
+bool isValidTopologicalOrder(std::vector<int> order, std::vector<int> jobs, std::vector<std::vector<int>> deps);
 #endif //
 
