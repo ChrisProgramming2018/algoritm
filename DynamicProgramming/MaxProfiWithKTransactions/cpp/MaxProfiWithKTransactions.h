ec32e42bebcc7dc58cf2fdca38a12132a21d4ba2
Christian Leininger <info2016frei@gmail.com>
2020-04-30 14:36:26 +0200

    add code

---
+++ DynamicProgramming/MaxProfiWithKTransactions/cpp/MaxProfiWithKTransactions.h
@@ -0,0 +1,16 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+int maxProfiWithKTransactions(std::vector<int> prices, int k);
+#endif //
+
