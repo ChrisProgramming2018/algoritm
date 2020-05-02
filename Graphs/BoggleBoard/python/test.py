c9f9c322c3e88bc072179e8ea725f9649783e4d3
Christian Leininger <info2016frei@gmail.com>
2020-05-02 09:57:41 +0200

    todo test

---
+++ Graphs/BoggleBoard/python/test.py
@@ -0,0 +1,180 @@
+# Copyright 2020
+# Author: Christian Leininger <info2016frei@gmail.com>
+{
+  "board": [
+    ["t", "h", "i", "s", "i", "s", "a"],
+    ["s", "i", "m", "p", "l", "e", "x"],
+    ["b", "x", "x", "x", "x", "e", "b"],
+    ["x", "o", "g", "g", "l", "x", "o"],
+    ["x", "x", "x", "D", "T", "r", "a"],
+    ["R", "E", "P", "E", "A", "d", "x"],
+    ["x", "x", "x", "x", "x", "x", "x"],
+    ["N", "O", "T", "R", "E", "-", "P"],
+    ["x", "x", "D", "E", "T", "A", "E"]
+  ],
+  "words": [
+    "this",
+    "is",
+    "not",
+    "a",
+    "simple",
+    "boggle",
+    "board",
+    "test",
+    "REPEATED",
+    "NOTRE-PEATED"
+  ]
+}
+Test Case 2
+{
+  "board": [
+    ["y", "g", "f", "y", "e", "i"],
+    ["c", "o", "r", "p", "o", "u"],
+    ["j", "u", "z", "s", "e", "l"],
+    ["s", "y", "u", "r", "h", "p"],
+    ["e", "a", "e", "g", "n", "d"],
+    ["h", "e", "l", "s", "a", "t"]
+  ],
+  "words": [
+    "san",
+    "sana",
+    "at",
+    "vomit",
+    "yours",
+    "help",
+    "end",
+    "been",
+    "bed",
+    "danger",
+    "calm",
+    "ok",
+    "chaos",
+    "complete",
+    "rear",
+    "going",
+    "storm",
+    "face",
+    "epual",
+    "dangerous"
+  ]
+}
+Test Case 3
+{
+  "board": [
+    ["a", "b", "c", "d", "e"],
+    ["f", "g", "h", "i", "j"],
+    ["k", "l", "m", "n", "o"],
+    ["p", "q", "r", "s", "t"],
+    ["u", "v", "w", "x", "y"]
+  ],
+  "words": ["agmsy", "agmsytojed", "agmsytojedinhcbgl", "agmsytojedinhcbfl"]
+}
+Test Case 4
+{
+  "board": [["a", "b"], ["c", "d"]],
+  "words": ["abcd", "abdc", "acbd", "acdb", "adbc", "adcb", "abca"]
+}
+Test Case 5
+{
+  "board": [
+    ["f", "t", "r", "o", "p", "i", "k", "b", "o"],
+    ["r", "w", "l", "p", "e", "u", "e", "a", "b"],
+    ["j", "o", "t", "s", "e", "l", "f", "l", "p"],
+    ["s", "z", "u", "t", "h", "u", "o", "p", "i"],
+    ["k", "a", "e", "g", "n", "d", "r", "g", "a"],
+    ["h", "n", "l", "s", "a", "t", "e", "t", "x"]
+  ],
+  "words": [
+    "frozen",
+    "rotten",
+    "teleport",
+    "city",
+    "zutgatz",
+    "kappa",
+    "before",
+    "rope",
+    "obligate",
+    "annoying"
+  ]
+}
+Test Case 6
+{
+  "board": [
+    ["c", "o", "m"],
+    ["r", "p", "l"],
+    ["c", "i", "t"],
+    ["o", "a", "e"],
+    ["f", "o", "d"],
+    ["z", "r", "b"],
+    ["g", "i", "a"],
+    ["o", "a", "g"],
+    ["f", "s", "z"],
+    ["t", "e", "i"],
+    ["t", "w", "d"]
+  ],
+  "words": [
+    "commerce",
+    "complicated",
+    "twisted",
+    "zigzag",
+    "comma",
+    "foobar",
+    "baz",
+    "there"
+  ]
+}
+Test Case 7
+{
+  "board": [
+    ["c", "o", "m"],
+    ["r", "p", "l"],
+    ["c", "i", "t"],
+    ["o", "a", "e"],
+    ["f", "o", "d"],
+    ["z", "r", "b"],
+    ["g", "i", "a"],
+    ["o", "a", "g"],
+    ["f", "s", "z"],
+    ["t", "e", "i"],
+    ["t", "w", "d"]
+  ],
+  "words": [
+    "cr",
+    "oc",
+    "ml",
+    "iao",
+    "opo",
+    "zrb",
+    "big",
+    "fs",
+    "ogiagao",
+    "dwd",
+    "twt"
+  ]
+}
+Test Case 8
+{
+  "board": [
+    ["c", "o", "m"],
+    ["r", "p", "l"],
+    ["c", "i", "t"],
+    ["o", "a", "e"],
+    ["f", "o", "d"],
+    ["z", "r", "b"],
+    ["g", "i", "a"],
+    ["o", "a", "g"],
+    ["f", "s", "z"],
+    ["t", "e", "i"],
+    ["t", "w", "d"]
+  ],
+  "words": [
+    "comlpriteacoofziraagsizefttw",
+    "comlpriteacoofzirabagsizefottw",
+    "comlpriteacoofziraagsizefottw",
+    "comlpriteacoofzirabagsizeftttw"
+  ]
+}
+
+
+
+
