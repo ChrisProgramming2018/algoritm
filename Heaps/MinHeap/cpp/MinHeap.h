d399577ee8894dad4d47c40d25436d6eec4a38c2
Christian Leininger <info2016frei@gmail.com>
2020-05-20 19:28:03 +0200

    add h file

---
+++ Heaps/MinHeap/cpp/MinHeap.h
@@ -0,0 +1,17 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+
+
+#ifndef MINHEAP_H_
+#define MINHEAP_H_
+
+class MinHeap {
+ public:
+  std::vector<int> build(std::vector<int> array);
+};
+#endif  // MINHEAP_H_
+
