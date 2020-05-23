9122c43baeb41f22e84f2332099d7e3f7370c737
Christian Leininger <info2016frei@gmail.com>
2020-05-23 18:34:36 +0200

    add h file passes tests

--- Heaps/MinHeap/cpp/MinHeap.h
+++ Heaps/MinHeap/cpp/MinHeap.h
@@ -4,6 +4,7 @@
 
 
 #include <vector>
+#include <iostream>
 
 
 #ifndef MINHEAP_H_
@@ -11,10 +12,15 @@
 
 class MinHeap {
  public:
+  explicit MinHeap(std::vector<int> array);
   std::vector<int> build(std::vector<int> array);
+  void swapElements(int left, int right);
+  void show();
   int peek();
-  void insert(int value) {
+  void insert(int value);
   void siftDown(int currentIdx, int endIdx, std::vector<int> array);
+  void shiftUp(int idxCurrent);
+  int removeMin();
   std::vector<int> _heap;
 };
 #endif  // MINHEAP_H_
