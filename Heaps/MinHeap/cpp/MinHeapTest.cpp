d060c6e8f73d37b6c8296224fcb318d94887513a
Christian Leininger <info2016frei@gmail.com>
2020-05-23 18:03:36 +0200

    add tests

---
+++ Heaps/MinHeap/cpp/MinHeapTest.cpp
@@ -0,0 +1,24 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include "MinHeap.h"
+#include <gtest/gtest.h>
+#include <iostream>
+
+
+
+TEST(MinHeapTest, MinHeap) {
+  std::vector<int> array = {8,9,1,3};
+  MinHeap  minHeap(array);
+  ASSERT_EQ(minHeap.peek(), 1);
+  minHeap.insert(-8);
+  ASSERT_EQ(minHeap.peek(), -8);
+  ASSERT_EQ(minHeap.removeMin(), -8);
+  ASSERT_EQ(minHeap.peek(), 1);
+  minHeap.insert(8);
+  ASSERT_EQ(minHeap.peek(), 1);
+
+
+}
