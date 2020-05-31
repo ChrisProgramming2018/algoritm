948376847c2eeb30d3ff33b77be803fa3ff1844e
Christian Leininger <info2016frei@gmail.com>
2020-05-31 09:03:58 +0200

    add begin of quicksort

---
+++ Sort/QuickSort/cpp/Quicksort.h
@@ -0,0 +1,17 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <vector>
+
+
+#ifndef QUICKSORT_H_
+#define QUICKSORT_H_
+class QuickSort {
+  std::vector<int> _array;
+  bool _random;
+  QuickSort(std::vector<int> array, bool random);
+};
+#endif  // QUICKSORT_H_
+
