477e4784ff1ad26bb434aaad312dd6d5a4b33a0a
Christian Leininger <info2016frei@gmail.com>
2020-05-31 09:04:14 +0200

    add begin of quicksort

---
+++ Sort/QuickSort/cpp/Quicksort.cpp
@@ -0,0 +1,19 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include "./Quicksort.h"
+
+// ______________________________________________________________
+Quicksort::Quicksort(std::vector<int> array) {
+  _array = array;
+}
+
+// ______________________________________________________________
+std::vector<int> Quicksort::quicksort(std::vector<int> array) {
+}
+
+// ______________________________________________________________
+std::vector<int> Quicksort::divideSort(std::vector<int> array, int leftIdx, int rightIdx) {
+}
