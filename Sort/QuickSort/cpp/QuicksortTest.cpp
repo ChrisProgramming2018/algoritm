c78262a758fa3d4c3f032673a46c078b190bb413
Christian Leininger <info2016frei@gmail.com>
2020-05-31 09:04:34 +0200

    add begin of quicksort Test

---
+++ Sort/QuickSort/cpp/QuicksortTest.cpp
@@ -0,0 +1,10 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include <gtest/gtest.h>
+#include "./Quicksort.h"
+
+TEST(QuicksortTest, Quicksort) {
+}
