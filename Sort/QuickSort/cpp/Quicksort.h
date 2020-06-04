86e0ddd0a39644501f9920fcbc70d02ecb0781c6
Christian Leininger <info2016frei@gmail.com>
2020-06-04 20:35:02 +0200

    add h file passes test

--- Sort/QuickSort/cpp/Quicksort.h
+++ Sort/QuickSort/cpp/Quicksort.h
@@ -3,18 +3,21 @@
 // Author: Christian Leininger <info2016frei@gmail.com>
 
 
-#include <vector>
 #include <stdlib.h>  
+#include <vector>
 
 #ifndef QUICKSORT_H_
 #define QUICKSORT_H_
+
 class Quicksort {
  public:
   std::vector<int> _array;
+  int _pivot;
   bool _random;
   explicit Quicksort(std::vector<int> array);
   std::vector<int> quicksortMain(bool random);
-  void divideSort(int leftIdx, int rightIdx);
+  int divideSort(int leftIdx, int rightIdx);
+  void recursive(int leftIdx, int rightIdx);
   int randomNumber(int leftIdx, int rightIdx);
   void swap(int leftIdx, int rightIdx);
 };
